6d8a4ebcd5ad47bacf6f369b294d5c6a6ce855ad
Maxime Bouillot <maxime.bouillot@withings.com>
2022-01-28 20:29:09 +0100

    Made some helper functions.

--- src/read_csv.rs
+++ src/read_csv.rs
@@ -12,11 +12,43 @@ fn open_file_as_lines(path: String) -> Vec<String> {
     let raw_csv = fs::read_to_string(path).expect("Unable to open input file.");
     println!("{}", raw_csv);
     println!("{}", raw_csv.len());
+    return split_to_vec(raw_csv, "\n".to_string());
+}
+
+/// This function is splits a string into a vector.
+fn split_to_vec(full_str: String, delimiter: String) -> Vec<String> {
     let mut ret: Vec<String> = Vec::new();
-    for s in raw_csv.split("\n") {
+    for s in full_str.split(&delimiter) {
         ret.push(s.to_string())
     }
     return ret;
 }
 
+
+/// Removes a copy of the string without trailing or leading whitespace.
+fn remove_leading_trailing_whitespace(s: String) -> String {
+    let mut ret: String = String::new();
+    // Removing leading whitespace
+    let mut not_leading_whitespace = false;
+    for c in s.chars() {
+        if !c.is_whitespace() {
+            not_leading_whitespace = true;
+        }
+        if not_leading_whitespace {
+            ret.push(c)
+        }
+    }
+    // Removing trailing whitespace
+    loop {
+        let last_char = match ret.pop() {
+            Some(c) => c,
+            None => return ret,
+        };
+        if !last_char.is_whitespace() {
+            ret.push(last_char);
+            break;
+        }
+    }
+    return ret;
+}
 
