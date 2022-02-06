64c0067b6c4e333e7ddb60082035023c962812b8
Maxime Bouillot <maxime.bouillot@withings.com>
2022-02-06 14:13:44 +0100

    Added new tests.

--- src/test.rs
+++ src/test.rs
@@ -4,7 +4,7 @@ use crate::write_md::gen_md_table;
 use crate::easy_io::split_to_vec;
 
 #[test]
-pub fn basic_table() {
+fn basic_table() {
     let basic_table = "1, 2\n3, 4";
     let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
 
@@ -17,3 +17,30 @@ pub fn basic_table() {
     }
 }
 
+#[test]
+fn testing_trailing_lines() {
+    let basic_table = "1, 2\n3, 4\n\n\n";
+    let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
+
+    let table_lines = split_to_vec(&basic_table, "\n");
+    let vectorized_table = read_csv_lines(&table_lines, ",");
+    let generated_table = gen_md_table(&vectorized_table);
+    if generated_table != ref_table {
+        println!("Expected:\n{}\nGot:{}\n", ref_table, generated_table);
+        assert_eq!(ref_table, generated_table);
+    }
+}
+
+#[test]
+fn testing_trailing_columns() {
+    let basic_table = "1, 2, , , , , \n3, 4, , , , ";
+    let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
+
+    let table_lines = split_to_vec(&basic_table, "\n");
+    let vectorized_table = read_csv_lines(&table_lines, ",");
+    let generated_table = gen_md_table(&vectorized_table);
+    if generated_table != ref_table {
+        println!("Expected:\n{}\nGot:{}\n", ref_table, generated_table);
+        assert_eq!(ref_table, generated_table);
+    }
+}
