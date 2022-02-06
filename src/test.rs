2b586fc1ebe6e44edd9c67d9f172ea65abc7f2fa
Maxime Bouillot <maxime.bouillot@withings.com>
2022-02-06 14:33:03 +0100

    Removed duplicated code.

--- src/test.rs
+++ src/test.rs
@@ -7,40 +7,31 @@ use crate::easy_io::split_to_vec;
 fn basic_table() {
     let basic_table = "1, 2\n3, 4";
     let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
-
-    let table_lines = split_to_vec(&basic_table, "\n");
-    let vectorized_table = read_csv_lines(&table_lines, ",");
-    let generated_table = gen_md_table(&vectorized_table);
-    if generated_table != ref_table {
-        println!("Expected:\n{}\nGot:{}\n", ref_table, generated_table);
-        assert_eq!(ref_table, generated_table);
-    }
+    cmp_csv_md(basic_table, ref_table);
 }
 
 #[test]
 fn testing_trailing_lines() {
     let basic_table = "1, 2\n3, 4\n\n\n";
     let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
-
-    let table_lines = split_to_vec(&basic_table, "\n");
-    let vectorized_table = read_csv_lines(&table_lines, ",");
-    let generated_table = gen_md_table(&vectorized_table);
-    if generated_table != ref_table {
-        println!("Expected:\n{}\nGot:{}\n", ref_table, generated_table);
-        assert_eq!(ref_table, generated_table);
-    }
+    cmp_csv_md(basic_table, ref_table);
 }
 
 #[test]
 fn testing_trailing_columns() {
     let basic_table = "1, 2, , , , , \n3, 4, , , , ";
     let ref_table = "|1|2|\n|-|-|\n|3|4|\n";
+    cmp_csv_md(basic_table, ref_table);
+}
 
-    let table_lines = split_to_vec(&basic_table, "\n");
+/// This functions tries to compare a CSV string and a md table.
+fn cmp_csv_md(csv: &str, md: &str) {
+    let table_lines = split_to_vec(&csv, "\n");
     let vectorized_table = read_csv_lines(&table_lines, ",");
     let generated_table = gen_md_table(&vectorized_table);
-    if generated_table != ref_table {
-        println!("Expected:\n{}\nGot:{}\n", ref_table, generated_table);
-        assert_eq!(ref_table, generated_table);
+    if generated_table != md {
+        println!("Expected:\n{}\nGot:{}\n", md, generated_table);
+        assert_eq!(md, generated_table);
     }
 }
+
